76b9175d8720b53ca52070781c0abd8a61e6aec9
anispwyn <anisphia_wynn_palettia@proton.me>
2026-03-06 05:06:01 +0700

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,72 @@
+{
+  inputs = {
+    self.submodules = true;
+    flakelight.url = "github:nix-community/flakelight";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    flakelight,
+    crane,
+    fenix,
+    ...
+  }:
+    flakelight ./. {
+      # make you able to access pkgs.fenix.complete.xyz
+      withOverlays = [
+        fenix.overlays.default
+      ];
+
+      packages.default = {pkgs, ...}: let
+        craneLib = (crane.mkLib pkgs).overrideToolchain (
+          p:
+            p.fenix.complete.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+            ]
+        );
+      in
+        craneLib.buildPackage {
+          pname = "repak-rivals"; # i added this so crane wont spam my fucking terminal
+          doCheck = false; # disable tests
+          src = ./.;
+          nativeBuildInputs = with pkgs; [
+            stdenv.cc.cc.lib
+            makeWrapper
+          ];
+
+          env = {
+            LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib"; # unsure if needed or not
+          };
+
+          # banger postInstall right here
+          postInstall = ''
+            wrapProgram $out/bin/repak-gui \
+              --prefix LD_LIBRARY_PATH : ${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.libX11}/lib:${pkgs.libXcursor}/lib:${pkgs.libxi}/lib:${pkgs.libxkbcommon}/lib:${pkgs.mesa}/lib:${pkgs.libGL}/lib
+          '';
+        };
+      apps.default = packages: {
+        type = "app";
+        program = "${packages.default}/bin/repak-gui";
+      };
+      devShell.packages = {pkgs, ...}: [
+        (pkgs.fenix.complete.withComponents [
+          "cargo"
+          "clippy"
+          "rust-src"
+          "rustc"
+          "rustfmt"
+          "rust-analyzer"
+        ])
+      ];
+    };
+}
